d3382f83fc2d3721853073a515481e44f288b994
glowfi <86939591+glowfi@users.noreply.github.com>
2022-12-10 00:18:38 +0530

    testvq

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
+// import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
