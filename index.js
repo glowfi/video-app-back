05c6b013b490076d5afe6023721d7d60a295f008
glowfi <86939591+glowfi@users.noreply.github.com>
2022-12-10 00:25:15 +0530

    testv3

--- index.js
+++ index.js
@@ -10,10 +10,8 @@ import CommentRoutes from './routes/comments.js';
 import UserRoutes from './routes/users.js';
 import VideoRoutes from './routes/videos.js';
 
-// const __filename = url.fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const app = express();
 
-import cors from 'cors';
 app.use(
     cors({
         credentials: true,
@@ -22,14 +20,11 @@ app.use(
     })
 );
 
-// const PORT =
-const app = express();
 dotenv.config();
 const connect = async () => {
     await mongoose.connect(process.env.MONGO);
 };
 
-app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use('/api/auth', AuthRoute);
